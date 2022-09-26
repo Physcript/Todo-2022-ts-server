2478869069625716e3e6efe0f8ae84900799f3ec
physcript <physcript@gmail.com>
2022-09-26 19:15:29 +0800

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const app = express()
+
+
+app.get('/test', (req, res) => {
+	res.send('Hello from backend')
+})
+
+app.listen(8080, () => {
+	console.log('Server listening: 8080')
+})
+
