fec40982bd91eebcc1e4ac247a0fede542199c57
physcript <physcript@gmail.com>
2022-09-29 00:43:57 +0800

    fixed json error

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,40 @@
 
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+const corsOptions = { origin: 'http://localhost:5174', credential: true }
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(corsOptions))
+
+
+
+const fakeTodos = [
+	{
+		id: '123',
+		text: 'go to groceries store',
+		isCompleted: false,
+	},
+	{
+		id: '234',
+		text: 'learn full stack development',
+		isCompleted: true
+	}
+]
 
 app.get('/test', (req, res) => {
 	res.send('Hello from backend')
+})
+
+
+app.get('/todos', (req, res) => {
+	res.status(200).json({
+		message: {
+			todo: fakeTodos
+		}
+	})
 })
 
 app.listen(8080, () => {
